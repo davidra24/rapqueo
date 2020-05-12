12bf4505a1a48d0dd166e08ff8411cb78ec69cda
David Ramírez <cdavidramirezg24@gmail.com>
2020-05-11 22:30:45 -0500

    filter

--- frontend/src/components/promos/index.vue
+++ frontend/src/components/promos/index.vue
@@ -58,7 +58,7 @@ export default {
       this.$router.push({ path: `/promociones` });
     },
     validarPromos() {
-      return this.filteredPromos;
+      return this.filteredPromos && this.filteredPromos.length;
     }
   }
 };
