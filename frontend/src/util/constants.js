377ce1b0eced338923a7f510cc319cb5d716406a
David Ramírez <cdavidramirezg24@gmail.com>
2020-05-27 19:14:22 -0500

    ..

--- frontend/src/util/constants.js
+++ frontend/src/util/constants.js
@@ -1,30 +1,30 @@
 //DESARROLLO
-export const apiUrl = "http://localhost:3000";
+//export const apiUrl = "http://localhost:3000";
 //PRUEBAS
 //export const apiUrl = 'http://192.168.100.62:3000';
 //PRODUCCIÓN
-//export const apiUrl = "https://mercarchevere.herokuapp.com";
-export const categories = "api/categories";
-export const products = "api/products";
-export const productsByCategorie = "api/productsByCategorie";
-export const promos = "api/promos";
-export const filteredPromos = "api/filteredPromos";
-export const signup = "api/signup";
-export const login = "api/login";
-export const verifyPassword = "api/verifyPassword";
-export const usuarios = "api/users";
-export const pedidos = "api/orders";
-export const notificationRegister = "notification/subscribe";
-export const productosSinCategoria = "api/productsNoCategorie";
-export const ordersByUser = "api/ordersByUser";
-export const updateOrder = "api/updateStateOrder";
-export const ordersByState = "api/ordersByState";
-export const forgot = "api/forgot";
-export const codeRecovery = "api/code-recovery";
-export const recoveryPassword = "api/recoveryPassword";
-export const productosSinPromo = "api/products-no-promo";
-export const userByPhone = "api/userByPhone";
-export const aditionals = "api/aditional";
+export const apiUrl = 'https://mercarchevere.herokuapp.com';
+export const categories = 'api/categories';
+export const products = 'api/products';
+export const productsByCategorie = 'api/productsByCategorie';
+export const promos = 'api/promos';
+export const filteredPromos = 'api/filteredPromos';
+export const signup = 'api/signup';
+export const login = 'api/login';
+export const verifyPassword = 'api/verifyPassword';
+export const usuarios = 'api/users';
+export const pedidos = 'api/orders';
+export const notificationRegister = 'notification/subscribe';
+export const productosSinCategoria = 'api/productsNoCategorie';
+export const ordersByUser = 'api/ordersByUser';
+export const updateOrder = 'api/updateStateOrder';
+export const ordersByState = 'api/ordersByState';
+export const forgot = 'api/forgot';
+export const codeRecovery = 'api/code-recovery';
+export const recoveryPassword = 'api/recoveryPassword';
+export const productosSinPromo = 'api/products-no-promo';
+export const userByPhone = 'api/userByPhone';
+export const aditionals = 'api/aditional';
 //Service worker and notifications
 export const public_key =
-  "BNdocsBvG3mZu0FY2GUOD7kZsLykj5mnnroe49ws-vXAm-usT9WAUxORSJqmMWn-9E8N0-LPbIXYfFghauVkcoM";
+  'BNdocsBvG3mZu0FY2GUOD7kZsLykj5mnnroe49ws-vXAm-usT9WAUxORSJqmMWn-9E8N0-LPbIXYfFghauVkcoM';
