14308dfe45ada353e92a338e341ee743f3728f73
Juan <juram-09@hotmail.com>
2021-01-04 11:20:09 -0500

    Servicios(?) 2.0

--- backend/models/Adicional.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const adicional = new Schema({
-  precio: Number,
-});
-
-module.exports = mongoose.model("adicional", adicional);
--- backend/models/Roles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const rol = new Schema({
-    nombre: String,
-    permiso: Array
-});
-
-module.exports = mongoose.model('rol', rol);
